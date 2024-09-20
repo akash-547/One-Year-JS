6b05f1bf7d015dad6f555a813e54f4d08f6a8e05
akash-547 <kashhussain512@gmail.com>
2024-09-20 10:41:21 +0500

    add third day

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
-console.log("Hello,World!")
-let a =2;
-let b=6;
-const addNumbers=a+b;
-console.log(addNumbers)
+// console.log("Hello,World!")
+
+// day two
+// let a =2;
+// let b=6;
+// const addNumbers=a+b;
+// console.log(addNumbers)
+
+//Day 3
+let number =11;
+if(number %2==0){
+    console.log('even number')
+}else{
+    console.log("this is an odd number")
+}
