b10e4ad34fb06e200812de0f976abf3ab9c745c0
akash-547 <kashhussain512@gmail.com>
2024-09-21 23:36:59 +0500

    add fourht day

--- main.js
+++ main.js
@@ -7,9 +7,22 @@
 // console.log(addNumbers)
 
 //Day 3
-let number =11;
-if(number %2==0){
-    console.log('even number')
+// let number =11;
+// if(number %2==0){
+//     console.log('even number')
+// }else{
+//     console.log("this is an odd number")
+// }
+
+// Day four
+// Question 4: Write a JavaScript program that takes two numbers as input and compares them. If the first number is greater, it should display "First number is greater." If the second number is greater, it should display "Second number is greater." If both are equal, it should display "Both numbers are equal."
+
+let fistNumber =100;
+let secondNumber = 500;
+if (fistNumber > secondNumber){
+    console.log("first number is great")
+}else if(secondNumber>fistNumber){
+    console.log("second Number is greater")
 }else{
-    console.log("this is an odd number")
+    console.log("both numbers are equal")
 }
